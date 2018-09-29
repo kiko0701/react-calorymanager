05206c9bdd8bf26dd75c8fc7cdf2e4b90919c915
kiko0701 <sol.tidus.rr@gmail.com>
2018-09-29 12:55:37 +0900

    キャッシュ削除

--- firebase/config.js
+++
@@ -1,8 +0,0 @@
-export default firebaseConfig = {
-    apiKey: "AIzaSyCqMjURZrRq2gC2tiQ5BEuq-QLZ641z13g",
-    authDomain: "react-calorie-manage.firebaseapp.com",
-    databaseURL: "https://react-calorie-manage.firebaseio.com",
-    projectId: "react-calorie-manage",
-    storageBucket: "react-calorie-manage.appspot.com",
-    messagingSenderId: "191316632907"
-};
--- src/action/actions.jsx
+++
@@ -1,5 +0,0 @@
-export default function addCalorie(food){
-    return {
-        type: food
-    }
-};
